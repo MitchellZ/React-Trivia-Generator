eb07fa09bc64e64bd1bb7df3f87e5d7e5c19eb63
MitchellZ <mitchellzoph@gmail.com>
2023-08-21 12:11:19 -0500

    Shuffle answers in answer key

--- Pages/Answers.tsx
+++ Pages/Answers.tsx
@@ -46,8 +46,8 @@ const Answers = () => {
 
   // Gets sets answers to answers from Firestore or indicates invalid ID
   function getAnswers() {
-    const answerKey = keys.find(x => x.id === id);
-  
+    const answerKey = keys.find((x) => x.id === id);
+
     // Check if the answer key exists for the given session ID
     if (!answerKey) {
       setAnswers(
@@ -57,44 +57,55 @@ const Answers = () => {
       );
       return;
     }
-  
+
     // Map over the results and create an element for each question and answer
     setAnswers(
       answerKey.answers.results.map((item, index) => {
-        let incorrectAnswers = null;
-  
-        // If the question type is not "boolean", render the incorrect answers
-        if (decodeURIComponent(item.type) !== "boolean") {
-          incorrectAnswers = item.incorrect_answers.map((answer, i) => (
-            <div key={`incorrect_div_${i}`}>
-            <p key={`incorrect_${i}`} className="incorrect">
-              {decodeURIComponent(answer)}
-            </p>
-            <br />
-            </div>
-          ));
-        }
-  
-        // Returns JSX elements representing trivia questions and answers
-        return (
-          <div key={`result_${index}`}>
-            <p key={`question_${index}`} className="question">
-              {index+1 + '. ' + decodeURIComponent(item.question)}
-            </p>
-            <br />
+        const isBooleanQuestion = decodeURIComponent(item.type) === "boolean";
+        const isCorrect = answer => answer === item.correct_answer;
+
+        // An array to store JSX answer elements
+        const answerElements = [];
+
+        // For boolean questions display only correct answer
+        if (isBooleanQuestion && isCorrect(item.correct_answer)) {
+          answerElements.push(
             <p key={`correct_${index}`} className="correct">
               {decodeURIComponent(item.correct_answer)}
             </p>
+          );
+        } else {
+          // Shuffle answers for non-boolean questions
+          const shuffledAnswers = [...item.incorrect_answers, item.correct_answer]
+            .sort(() => Math.random() - 0.5);
+
+          // Iterate through shuffled answers to create JSX elements
+          shuffledAnswers.forEach((answer, i) => {
+            answerElements.push(
+              <div key={`answer_${i}`} className={isCorrect(answer) ? "correct" : "incorrect"}>
+                <p>{decodeURIComponent(answer)}</p>
+                <br />
+              </div>
+            );
+          });
+        }
+    
+        // Finally return JSX elements representing the trivia questions and answers
+        return (
+          <div key={`result_${index}`}>
+            <p key={`question_${index}`} className="question">
+              {index + 1 + '. ' + decodeURIComponent(item.question)}
+            </p>
             <br />
-            {incorrectAnswers}
+            {answerElements}
             <div key={`spacer_${index}`} id="spacer" />
           </div>
         );
       })
-    );
+    );    
   }
-  
-   // Begin HTML Template
+
+  // Begin HTML Template
   return (
     <div>
       <div className="nav">
@@ -115,10 +126,10 @@ const Answers = () => {
         <div className="welcome">
           <h1>ANSWER KEY</h1>
         </div>
-        <div className='answer_key'>
-        <br />
+        <div className="answer_key">
+          <br />
           {answers}
-          </div>
+        </div>
       </div>
       <footer>
         <ul>
