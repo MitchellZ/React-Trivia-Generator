c088786bffdfebf2e0a4942298da7b2d952bb410
MitchellZ <mitchellzoph@gmail.com>
2023-10-16 14:27:49 -0500

    Refactor for transition to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Answers from './Pages/Answers';
+import Home from './Pages/Home.tsx';
+import About from './Pages/About.tsx';
+import Answers from './Pages/Answers.tsx';
 
 // Defining the default function for the App component
 export default function App() {
@@ -17,9 +17,9 @@ export default function App() {
       </div>
       <Routes>
         {/* Defining routes for the pages */}
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/About" element={<About />} />
-          <Route exact path="/Answers" element={<Answers />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Answers" element={<Answers />} />
       </Routes>
       <div>
       </div>
